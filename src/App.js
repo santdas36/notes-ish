c3d5784115ef2be15a2258ac2fef09f6f1d0dfcc
santdas36 <santdas36@gmail.com>
2020-08-18 18:56:23 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./bootstrap-reboot.css";
 import "./App.css";
 import Note from "./Note";
 
@@ -7,19 +8,21 @@ function App() {
   const [inputVal, setInputVal] = useState('');
   const handleAdd = (event) => {
 	event.preventDefault();
-     setTodos([...todos, inputVal]);
-	setInputVal('');
+	if (!inputVal) {
+     	setTodos([...todos, inputVal]);
+		setInputVal('');
+	}
   }
   return (
     <div className="app">
     		<div class="app__header">
 			<h1>TuDu</h1>
-			<div class="avatar"></div>
+			<div class="app__avatar"></div>
 		</div>
 		<div class="app__input">
 			<form>
 				<textarea value={inputVal} onChange={(event) => (setInputVal(event.target.value))} placeholder="Write here..."></textarea>
-				<button class="submit" onClick={handleAdd} type="submit">Add to Notes</button>
+				<button class="app__submit" onClick={handleAdd} type="submit">Add to Notes</button>
 			</form>
 		</div>
 		<div class="app__notes">
--- src/Note.js
+++ src/Note.js
@@ -5,16 +5,16 @@ function Note({ todo }) {
 	return (
 		<div class="note">
 			<div class="note__header">
-				<button class="edit">Edit</button>
-				<button class="delete">Delete</button>
+				<button class="note__edit">Edit</button>
+				<button class="note__delete">Delete</button>
 			</div>
 			<div class="note__body">
-				<h2 class="title">title</h2>
-				<p class="content">{todo}</p>
-				<p class="time">2.40pm Saturday</p>
+				<h2 class="note__title">title</h2>
+				<p class="note__content">{todo}</p>
+				<p class="note__time">2.40pm Saturday</p>
 			</div>
 			<div class="note__footer">
-				<button class="save">Save</button>
+				<button class="note__save">Save</button>
 			</div>
 		</div>	
 	);

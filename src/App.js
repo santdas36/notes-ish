70a38702cc396be81326c9fc488c826a29fa3ef6
santdas36 <santdas36@gmail.com>
2020-08-18 17:52:29 +0530

    /

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [ todos, setTodos ] = useState(['one', 'two', 'three']);
-  const [ inputVal, setInputVal ] = useState('');
+  const [todos, setTodos] = useState(['one', 'two', 'three']);
+  const [inputVal, setInputVal] = useState('');
   const handleAdd = () => {
-setTodos(...todos, inputVal);
-}
+    setTodos(...todos, inputVal);
+  }
   return (
     <div className="app">
-    <input value={inputVal} onChange={ (event) => {setInputVal(event.target.value)} }>
+    <input value={inputVal} onChange={ (event) => {setInputVal(event.target.value)} } />
 <button onClick={handleAdd}>Add</button>
 
       <ul>

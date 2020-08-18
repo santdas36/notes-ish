c85893fb06d6ccb648d0aceb40909bb1d7c916dd
santdas36 <santdas36@gmail.com>
2020-08-18 17:40:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [ todos, setTodos ] = useState(['one', 'two', 'three']);
+  const [ inputVal, setInputVal ] = useState('');
+  const handleAdd = () => {
+setTodos(...todos, inputVal);
+}
+  return (
+    <div className="app">
+    <input value={inputVal} onChange={ (event) => {setInputVal(event.target.value)} }>
+<button onClick={handleAdd}>Add</button>
+
+      <ul>
+        { todos?.map(todo => {
+            <li>{ todo }</li>
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;

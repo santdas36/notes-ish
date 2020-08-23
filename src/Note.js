adc81c6b40c0ec48752ae6c360c5ae736edf564f
santdas36 <santdas36@gmail.com>
2020-08-23 22:42:20 +0530

    ed v2

--- src/Note.js
+++ src/Note.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Note.css";
 import formatTime from "./formatTime.js";
 import db from "./firebase";
 
 function Note({ note, uid, id }) {
+	const [editable, setEditable] = useState(null);
+	const [title, setTitle] = useState('');
+
 	const handleDelete = () => {
 		db.collection('users').doc(uid).collection('notes').doc(id).delete();
 	}
@@ -11,11 +14,11 @@ function Note({ note, uid, id }) {
 	return (
 		<div className="note">
 			<div className="note__header">
-				<button className="note__edit" onClick={(event) => console.log(event)}>Edit</button>
+				<button className="note__edit" onClick={() => setEdit(true)}>Edit</button>
 				<button className="note__delete" onClick={handleDelete}>Delete</button>
 			</div>
 			<div className="note__body">
-				<h2 className="note__title">{note.note}</h2>
+				<h2 className="note__title" contenteditable={editable}>{note.note}</h2>
 				<p className="note__content">note</p>
 				<p className="note__time">{ note.time && formatTime('%l:%M%P - %b %d', new Date(note.time.toDate())) }</p>
 			</div>
